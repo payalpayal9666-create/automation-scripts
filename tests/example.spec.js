9e25bfd6967eb16b020e6947765d13e4de891523
payal <payal.payal9666@gmail.com>
2026-06-18 23:54:29 +0530

    i

---
+++ tests/example.spec.js
@@ -0,0 +1,13 @@
+const { test, expect } = require("@playwright/test");
+test("Login", async ({ page }) => {
+  await page.goto("https://www.aljaberoptical.com/");
+  await page.locator("#newSignin").click();
+  await page.locator("#userinput").waitFor({ state: "visible" });
+  await page.locator("#userinput").fill("payalchadhanirag@gmail.com");
+  await page.locator("[value='Proceed']").click();
+  await page.locator("text=Use Password?").waitFor({ state: "visible" });
+  await page.locator("text=Use Password?").click();
+  await page.locator("#password").fill("nirag@123");
+  await page.locator("form[name='f-signin'] button[type='submit']").click();
+  await page.waitForTimeout(5000);
+});
