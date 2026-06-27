9d8a6e009c80956d90773c4ea15ec894d6024fce
payal <payal.payal9666@gmail.com>
2026-06-28 02:13:30 +0530

    order placed

--- tests/automation.spec.js
+++ tests/automation.spec.js
@@ -3,13 +3,30 @@ test("add to cart", async ({ page }) => {
   await page.goto("https://automationexercise.com/");
   await page.locator("text= Products").waitFor({ state: "visible" });
   await page.locator('a[href="/products"]').click();
-  await page.getByText("Add to cart").first().click();
-  await page.locator('a[href="/view_cart"]').waitFor({ state: "visible" });
-  await page.locator('a[href="/view_cart"]').click();
-  await page.getByRole("button", { name: "Proceed To Checkout" }).click();
-  await page.locator('a[href="/login"]').click;
-  await page.locator('[data-qa = "login-email"]').fill("standard_user");
-  await page.locator('[data-qa="login-password"]').fill("secret_sauce");
+  await page.locator(".product-image-wrapper").first().hover();
+  await page.locator(".product-overlay .add-to-cart").first().click();
+  // await page.locator('a[href="/products"]').first().click();
+  // await firstProduct.hover();
+  // await page.getByText("Add to cart").first().click();
+  await page
+    .locator(".modal-body a[href='/view_cart']")
+    .waitFor({ state: "visible" });
+  await page.locator(".modal-body a[href='/view_cart']").click();
+  await page.locator(".check_out").click();
+  await page.getByRole("link", { name: "Register / Login" }).click();
+  await page
+    .locator('[data-qa = "login-email"]')
+    .fill("testunique@mailinator.com");
+  await page.locator('[data-qa="login-password"]').fill("test@123");
   await page.locator('[data-qa="login-button"]').click();
+  await page.locator("a[href='/view_cart']").first().click();
+  await page.getByText("Proceed To Checkout").click();
+  await page.getByRole("link", { name: "Place Order" }).click();
+  await page.locator('[data-qa = "name-on-card"]').fill("Test User");
+  await page.locator('[data-qa = "card-number"]').fill("4111111111111111");
+  await page.locator('[data-qa = "cvc"]').fill("311");
+  await page.locator('[data-qa = "expiry-month"]').fill("12");
+  await page.locator('[data-qa = "expiry-year"]').fill("2027");
+  await page.locator("#submit").click();
   await page.pause();
 });
